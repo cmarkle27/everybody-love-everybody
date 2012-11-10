ca807602cf43b228ec9b54280a374da632c8bb44
airportyh <airportyh@gmail.com>
2012-11-10 17:02:19 -0500

    Removed config for stylus.

--- server.js
+++ server.js
@@ -7,17 +7,8 @@ var app = express()
 app.configure(function(){
     app.use(app.router)
     app.use(express.static(path.join(__dirname, 'public')))
-    app.use(stylus.middleware({
-        src: __dirname + '/styl',
-        dest: __dirname + '/public/css',
-        compile: function (str, path, fn) { // optional, but recommended
-          stylus(str)
-          .set('filename', path)
-          .set('compress', true)
-          .render(fn);
-        }
-    }));
 })
 
 
 app.listen(3000)
+console.log('Server listening on port 3000')
